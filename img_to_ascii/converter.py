6fb5446974205a4091290c53b45631b0d5703b7e
Amir Alsayed <amyralsyd367@gmail.com>
2024-05-17 15:30:21 +0300

    interactive app

--- img_to_ascii/converter.py
+++ img_to_ascii/converter.py
@@ -2,32 +2,25 @@
 """
 convert any image to its representation in asccii like so:
 WIIL BE ADDED ASAP
+converter function that converts img to chars
 """
 import cv2 as cv
+from sys import argv
 
-
-def converter(*args):
-    """
-    converter function that converts img to chars
-    """
-    chars_str = " .:;oO8@#█"
-    img = cv.imread(args[0], cv.IMREAD_GRAYSCALE)
-    height, width = img.shape
-
-    if len(args) < 1:
-        print("must enter a file name")
-
-    with open('pixel_values.txt', 'w') as file:
-        for y in range(height):
-            for x in range(width):
-                gray_value = img[y, x]
-
-                idx = int((gray_value / 255) * (len(chars_str) - 1))
-                char = chars_str[idx]
-                file.write(char)
-            file.write('\n')
-    print("ascii img >> pixel_values.txt")
-
-
-
-converter("img2.jpg")
+if argv[3]:
+    print("step = {}".format(argv[3]))
+    step = int(argv[3] )
+else:
+    step = 5
+chars_str = " .:;oO8@#█"
+img = cv.imread(argv[1], cv.IMREAD_GRAYSCALE)
+height, width = img.shape
+with open('{}.txt'.format(argv[2]), 'w') as file:
+    for y in range(0, height, step):
+        for x in range(0, width, step):
+            gray_value = img[y, x]
+            idx = int((gray_value / 255) * (len(chars_str) - 1))
+            char = chars_str[idx]
+            file.write(char)
+        file.write('\n')
+print("ascii img >> {}.txt".format(argv[2]))
